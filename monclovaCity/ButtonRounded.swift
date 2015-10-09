c44fadb5321727888b1ea8483b9ccb5d73513629
Ramiro Martinez Chavez <ramiromartinezchavez@macbook-pro-de-ramiro.local>
2015-10-09 16:03:56 -0500

    Clase para modificar botones

---
+++ monclovaCity/ButtonRounded.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonRounded.swift
+//  monclovaCity
+//
+//  Created by Ramiro Martinez Chavez on 09/10/15.
+//  Copyright © 2015 Ramiro Martinez Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ButtonRounded: UIButton {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
