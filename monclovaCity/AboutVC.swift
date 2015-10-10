6d3d4f0777b7d9332f3f92aef12fd36db11579da
Ramiro Martinez Chavez <ramiromartinezchavez@MacBook-Pro-de-Ramiro.local>
2015-10-09 20:38:45 -0500

    files update

---
+++ monclovaCity/AboutVC.swift
@@ -0,0 +1,35 @@
+//
+//  AboutVC.swift
+//  monclovaCity
+//
+//  Created by Ramiro Martinez Chavez on 09/10/15.
+//  Copyright © 2015 Ramiro Martinez Chavez. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
